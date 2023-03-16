bc231396023103da83d5dbfbbf35ca9478ae8241
Hanny Kim <106687260+Gonnabee@users.noreply.github.com>
2023-03-16 10:09:11 +0900

    Add files via upload

---
+++ 4_1_number.py
@@ -0,0 +1,11 @@
+print(0)
+print('1+1', 1+1) 
+print('2-1',1)
+print('2*1', 2*1)
+
+import math
+print('math.sqrt(4)', math.sqrt(4))
+print('math.pow(4,2)', math.pow(4,2))
+
+import random
+print('random.random()', random.random())
---
+++ 4_2_text.py
@@ -0,0 +1,11 @@
+print('Hello world')
+print('''
+Hello
+world
+''')
+
+print("'1'+'1'", '1'+'1') #문자열과 문자열 결합하는 결합연산
+print('Hello world'*5)
+print(len('Hello world'))
+
+print('Hello world'.replace('world', 'universe'))
